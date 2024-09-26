788e99e8c9ea87c586701ba8bd0f5c83228f00d3
arman <armanhossain.tech@gmail.com>
2024-09-26 16:49:42 +0600

    introduce celery and redis with flower file and setup everything in settings

---
+++ biographer_api/celery.py
@@ -0,0 +1,12 @@
+import os
+
+from celery import Celery
+from django.conf import settings
+
+# TODO: change this in production
+os.environ.setdefault(key="DJANGO_SETTINGS_MODULE", value="biographer_api.settings")
+
+app = Celery("biographer_api")
+app.config_from_object("django.conf:settings", namespace="CELERY")
+
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
