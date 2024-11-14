aff4d6627130295834fb69f5c513b1f948b0a5e6
arman <armanhossain.tech@gmail.com>
2024-11-14 20:34:43 +0600

    update follow unfollow profile logic view

--- core_apps/profiles/views.py
+++ core_apps/profiles/views.py
@@ -98,7 +98,7 @@ class FollowingListAPIView(APIView):
 class FollowAPIView(APIView):
     def post(self, request, user_id, format=None):
         try:
-            follower = Profile.objects.get(user=self.request.user)
+            follower = Profile.objects.get(user__id=self.request.user.id)
             user_profile = request.user.user_profile
             profile = Profile.objects.get(user__id=user_id)
 
@@ -123,8 +123,7 @@ class FollowAPIView(APIView):
             send_mail(subject, message, from_email, recipient_list, fail_silently=True)
             return Response({
                 "status_code": status.HTTP_200_OK,
-                "message": f"You are now following {profile.user.first_name} "
-                           f"{profile.user.last_name}"
+                "message": f"You are now following {profile.user.first_name.title()} {profile.user.last_name.title()}"
             }, status=status.HTTP_200_OK)
 
         except Profile.DoesNotExist:
@@ -139,16 +138,15 @@ class UnfollowAPIView(APIView):
         if not user_profile.check_following(profile):
             formatted_response = {
                 "status_code": status.HTTP_400_BAD_REQUEST,
-                "message": f"You can't unfollow {profile.user.first_name}"
-                           f"{profile.user.last_name}, since you were not following then in the first palce."
+                "message": f"You can't unfollow {profile.user.first_name} "
+                           f"{profile.user.last_name}, since you were not following then in the first place."
             }
             return Response(formatted_response, status=status.HTTP_400_BAD_REQUEST)
 
         user_profile.unfollow(profile)
         formatted_response = {
             "status_code": status.HTTP_200_OK,
-            "message": f"You have unfollowed {profile.user.first_name}"
-                       f"{profile.user.last_name}"
+            "message": f"You have unfollowed {profile.user.first_name.title()} {profile.user.last_name.title()}"
         }
         return Response(formatted_response, status=status.HTTP_200_OK)
 
