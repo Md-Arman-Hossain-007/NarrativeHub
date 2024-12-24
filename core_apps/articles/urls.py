01cf73425da12d5e1a2f69c7cdd93e1adeb6b614
arman <armanhossain.tech@gmail.com>
2024-12-24 17:08:30 +0600

    added claps feature with api and serializer

--- core_apps/articles/urls.py
+++ core_apps/articles/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import ArticleListCreateAPIView, ArticleRetrieveUpdateDestroyAPIView
+from .views import ArticleListCreateAPIView, ArticleRetrieveUpdateDestroyAPIView, ClapArticleAPIView
 
 urlpatterns = [
     path("", ArticleListCreateAPIView.as_view(), name="article-list-create"),
     path("<uuid:id>/", ArticleRetrieveUpdateDestroyAPIView.as_view(), name="article-retrieve-update-destroy"),
+    path("<uuid:article_id>/clap/", ClapArticleAPIView.as_view(), name="clap-article"),
 ]
