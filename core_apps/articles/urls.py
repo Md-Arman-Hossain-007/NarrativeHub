1a7f9249c574287e0da6f674a382200c04c9bbd9
Arman <armanhossain.tech@gmail.com>
2025-02-06 18:05:35 +0600

    Committed core_apps/articles/urls.py

--- core_apps/articles/urls.py
+++ core_apps/articles/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 
-from .views import ArticleListCreateAPIView, ArticleRetrieveUpdateDestroyAPIView, ClapArticleAPIView
+from .views import (
+    ArticleListCreateAPIView,
+    ArticleRetrieveUpdateDestroyAPIView,
+    ClapArticleAPIView,
+)
 
 urlpatterns = [
     path("", ArticleListCreateAPIView.as_view(), name="article-list-create"),
-    path("<uuid:id>/", ArticleRetrieveUpdateDestroyAPIView.as_view(), name="article-retrieve-update-destroy"),
+    path(
+        "<uuid:id>/",
+        ArticleRetrieveUpdateDestroyAPIView.as_view(),
+        name="article-retrieve-update-destroy",
+    ),
     path("<uuid:article_id>/clap/", ClapArticleAPIView.as_view(), name="clap-article"),
 ]
