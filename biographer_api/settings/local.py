bf6b61f59bf546686396b07e792aaba3be337a16
Arman <armanhossain.tech@gmail.com>
2025-02-06 18:05:34 +0600

    Committed biographer_api/settings/local.py

--- biographer_api/settings/local.py
+++ biographer_api/settings/local.py
@@ -1,10 +1,10 @@
-from .base import * # noqa
+from .base import *  # noqa
 from .base import env
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env(
-        "DJANGO_SECRET_KEY",
-        default="XXgyo8OVUPGLK3wm6drzlzx3mLIIYEoWa_71qyAs-8oGtrDtrgw",
+    "DJANGO_SECRET_KEY",
+    default="XXgyo8OVUPGLK3wm6drzlzx3mLIIYEoWa_71qyAs-8oGtrDtrgw",
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
