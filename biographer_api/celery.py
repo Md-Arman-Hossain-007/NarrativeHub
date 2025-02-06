56229b97f288c385d4e7a06202793dde50c8ccc3
Arman <armanhossain.tech@gmail.com>
2025-02-06 18:05:34 +0600

    Committed biographer_api/celery.py

--- biographer_api/celery.py
+++ biographer_api/celery.py
@@ -4,7 +4,9 @@ from celery import Celery
 from django.conf import settings
 
 # TODO: change this in production
-os.environ.setdefault(key="DJANGO_SETTINGS_MODULE", value="biographer_api.settings.local")
+os.environ.setdefault(
+    key="DJANGO_SETTINGS_MODULE", value="biographer_api.settings.local"
+)
 
 app = Celery("biographer_api")
 app.config_from_object("django.conf:settings", namespace="CELERY")
