71313e531d7eb0847408d42beeff1baeb0cd117f
Arman <armanhossain.tech@gmail.com>
2025-02-06 18:05:35 +0600

    Committed core_apps/profiles/exceptions.py

--- core_apps/profiles/exceptions.py
+++ core_apps/profiles/exceptions.py
@@ -3,5 +3,5 @@ from rest_framework.exceptions import APIException
 
 class CantFollowYourself(APIException):
     status_code = 403
-    default_detail = 'You cannot follow yourself.'
-    default_code = 'forbidden'
+    default_detail = "You cannot follow yourself."
+    default_code = "forbidden"
