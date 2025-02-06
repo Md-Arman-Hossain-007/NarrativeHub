304234a63fe6ffce3e52dc43f78f441e93af9838
Arman <armanhossain.tech@gmail.com>
2025-02-06 18:05:35 +0600

    Committed core_apps/users/admin.py

--- core_apps/users/admin.py
+++ core_apps/users/admin.py
@@ -11,42 +11,47 @@ class UserAdmin(BaseUserAdmin):
     add_form = UserCreationForm
     model = User
 
-    list_display = ["pkid", "id", "email", "first_name", "last_name", "is_active", "is_staff", "date_joined"]
+    list_display = [
+        "pkid",
+        "id",
+        "email",
+        "first_name",
+        "last_name",
+        "is_active",
+        "is_staff",
+        "date_joined",
+    ]
 
     list_display_links = ["pkid", "id", "email"]
 
     list_filter = ["email", "is_active", "is_staff"]
 
     fieldsets = (
-        (_("Login Credentials"), {
-            "fields": ("email", "password")
-        }),
-        (_("Personal Info"), {
-            "fields": ("first_name", "last_name")
-        }),
-        (_("Permissions and Groups"), {
-            "fields": (
-                "is_active",
-                "is_staff",
-                "is_superuser",
-                "groups",
-                "user_permissions"
-            )
-        }),
-        (_("Important Dates"), {
-            "fields": ("last_login", "date_joined")
-        }),
+        (_("Login Credentials"), {"fields": ("email", "password")}),
+        (_("Personal Info"), {"fields": ("first_name", "last_name")}),
+        (
+            _("Permissions and Groups"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (_("Important Dates"), {"fields": ("last_login", "date_joined")}),
     )
 
     add_fieldsets = (
         None,
         {
             "classes": ("wide",),
-            "fields": ("email", "first_name", "last_name", "password1", "password12")
-        }
+            "fields": ("email", "first_name", "last_name", "password1", "password2"),
+        },
     )
     search_fields = ["email", "first_name", "last_name"]
 
 
 admin.site.register(User, UserAdmin)
-
