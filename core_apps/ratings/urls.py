5b1ac53d6549c96a779b59d38ff740c71db8669e
Arman <armanhossain.tech@gmail.com>
2025-02-06 18:05:35 +0600

    Committed core_apps/ratings/urls.py

--- core_apps/ratings/urls.py
+++ core_apps/ratings/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from .views import RatingCreateAPIView
 
 urlpatterns = [
-    path("rate-article/<uuid:article_id>/", RatingCreateAPIView.as_view(), name="rating-create"),
+    path(
+        "rate-article/<uuid:article_id>/",
+        RatingCreateAPIView.as_view(),
+        name="rating-create",
+    ),
 ]
