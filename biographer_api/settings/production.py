45d8d42a231f87c1b979a19ee3d4c742243bd16d
Arman <armanhossain.tech@gmail.com>
2025-02-06 18:05:35 +0600

    Committed biographer_api/settings/production.py

--- biographer_api/settings/production.py
+++ biographer_api/settings/production.py
@@ -1,7 +1,9 @@
 from .base import *  # noqa
 from .base import env
 
-ADMINS = [("Md. Arman Hossain", "armanhossain.tech@gmail.com")]  # if any errors rise send to this email in prod
+ADMINS = [
+    ("Md. Arman Hossain", "armanhossain.tech@gmail.com")
+]  # if any errors rise send to this email in prod
 
 # Todo: add domain name of the production
 CORS_TRUSTED_ORIGINS = [""]
