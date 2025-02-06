8e1dd2ccbfa9002074eb015410cd4dd206c27d7c
Arman <armanhossain.tech@gmail.com>
2025-02-06 18:05:35 +0600

    Committed core_apps/users/managers.py

--- core_apps/users/managers.py
+++ core_apps/users/managers.py
@@ -24,32 +24,29 @@ class CustomUserManager(BaseUserManager):
             raise ValueError(_("Users must have an email address"))
 
         user = self.model(
-                first_name=first_name,
-                last_name=last_name,
-                email=email,
-                **extra_fields
+            first_name=first_name, last_name=last_name, email=email, **extra_fields
         )
 
         user.set_password(password)
 
-        extra_fields.setdefault('is_staff', False)
-        extra_fields.setdefault('is_superuser', False)
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
 
         user.save(using=self._db)
         return user
 
     def create_superuser(self, first_name, last_name, email, password, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
 
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
 
         if not password:
-            raise ValueError('Superuser must have a password.')
+            raise ValueError("Superuser must have a password.")
 
         if email:
             email = self.normalize_email(email)
